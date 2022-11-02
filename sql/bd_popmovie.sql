a892ff678ac2d2945120b656a785a480d6b51d5e
Gabriel2712 <gabrielcabral952@gmail.com>
2022-11-02 15:03:03 -0300

    inserindo sql de criação do banco e importação dos dados

---
+++ sql/bd_popmovie.sql
@@ -0,0 +1,28 @@
+## APENAS ESBOÇO INICIAL, IDEAL REFINAR/MELHORAR ESTRUTURA DA TABELA EXISTENTE E CRIAR NOVAS
+CREATE DATABASE IF NOT EXISTS bd_popmovie;
+USE bd_popmovie;
+
+SELECT * FROM tb_filme;
+CREATE TABLE IF NOT EXISTS tb_filme(
+    id int PRIMARY KEY AUTO_INCREMENT,
+    nome varchar(40),
+    ano_lancamento int NOT NULL,
+    classificacao varchar(3) NOT NULL,
+    duracao_min int NOT NULL,
+    genero varchar(50) NOT NULL,
+    nota_imdb double NOT NULL,
+    sinopse varchar(500) NOT NULL,
+    diretor varchar(50) NOT NULL
+);
+
+## COMANDO PARA PEGAR DADOS DE ARQUIVO CSV E INSERIR NO BANCO
+LOAD DATA INFILE 'imdb_movies.csv' ## CAMINHO DE ONDE SE ENCONTRA O ARQUIVO CSV, IDEAL ESPECIFICAR POR COMPLETO, EX: C:/../../../nomeplanilha.csv || OUTRA OPÇÃO É DEIXAR A PLANILHA DENTRO DA PASTA DO BANCO (../mysql/data/bd_popmovie/planilha.csv), PASSANDO A SER NECESSÁRIO SÓ INFORMAR O ARQUIVO DA PLANILHA SEM O CAMINHO NO COMANDO
+INTO TABLE tb_filme
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 2 lines ## Pular duas primeiras linhas da planilha
+(nome, ano_lancamento, classificacao, duracao_min, genero, nota_imdb, sinopse, diretor)
+
+## BASE DE DADOS INICIAL: https://www.kaggle.com/datasets/ramjasmaurya/top-250s-in-imdb  -> Caso acharmos melhores, podemos substituir
+
