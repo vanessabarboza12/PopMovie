42de29f1975ae2c6aed4ae3bb3b4d5c6072887d5
Gabriel2712 <gabrielcabral952@gmail.com>
2022-11-10 23:58:03 -0300

    Telas de 'telespectador' e 'administrador' renomeadas conforme o padrão

---
+++ PopMovie/FormTelespectador.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PopMovie
+{
+    public partial class FormTelespectador : Form
+    {
+        public FormTelespectador()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buscar_btn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
