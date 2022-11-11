e2155162d3e0aa8b95b80dee402f462b36b7ff43
Gabriel2712 <gabrielcabral952@gmail.com>
2022-11-10 23:52:13 -0300

    Classe usuario desmembrada para 'telespectador' e 'administrador'

---
+++ PopMovie/Administrador.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PopMovie
+{
+    public class Administrador
+    {
+        private string nome;
+        private string email;
+        private string senha;
+
+        public Administrador(string nome, string email, string senha)
+        {
+            this.nome = nome;
+            this.email = email;
+            this.senha = senha;
+        }
+
+        public string getNome() { return nome; }
+        public string getEmail() { return email; }
+        public string getSenha() { return senha; }
+
+
+    }
+}
