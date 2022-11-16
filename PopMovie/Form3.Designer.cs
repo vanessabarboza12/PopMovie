c3918446e470d25cb6a47be344ac53eda84a0e77
Gustavo Batista <guuhjb@gmail.com>
2022-11-16 20:46:30 -0300

    Novas telas -> Gerenciar Contas

---
+++ PopMovie/Form3.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace PopMovie
+{
+    partial class GerenciarConta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.txbUserNameConta = new System.Windows.Forms.TextBox();
+            this.txbEmailConta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateNascimento_Conta = new System.Windows.Forms.DateTimePicker();
+            this.btnVoltar = new System.Windows.Forms.Button();
+            this.btnRemoverConta = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(18, 235);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(275, 31);
+            this.label2.TabIndex = 24;
+            this.label2.Text = "Data de Nascimento: ";
+            // 
+            // txbUserNameConta
+            // 
+            this.txbUserNameConta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbUserNameConta.Location = new System.Drawing.Point(341, 170);
+            this.txbUserNameConta.Name = "txbUserNameConta";
+            this.txbUserNameConta.Size = new System.Drawing.Size(374, 29);
+            this.txbUserNameConta.TabIndex = 23;
+            // 
+            // txbEmailConta
+            // 
+            this.txbEmailConta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbEmailConta.Location = new System.Drawing.Point(341, 107);
+            this.txbEmailConta.Name = "txbEmailConta";
+            this.txbEmailConta.Size = new System.Drawing.Size(374, 29);
+            this.txbEmailConta.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(18, 170);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(232, 31);
+            this.label5.TabIndex = 21;
+            this.label5.Text = "Nome do Usuário:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(18, 107);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 31);
+            this.label4.TabIndex = 20;
+            this.label4.Text = "E-mail: ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 44.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(494, 67);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Gerenciar Contas";
+            // 
+            // dateNascimento_Conta
+            // 
+            this.dateNascimento_Conta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateNascimento_Conta.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateNascimento_Conta.Location = new System.Drawing.Point(341, 238);
+            this.dateNascimento_Conta.Name = "dateNascimento_Conta";
+            this.dateNascimento_Conta.Size = new System.Drawing.Size(374, 29);
+            this.dateNascimento_Conta.TabIndex = 27;
+            this.dateNascimento_Conta.Value = new System.DateTime(2000, 1, 1, 0, 0, 0, 0);
+            // 
+            // btnVoltar
+            // 
+            this.btnVoltar.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnVoltar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnVoltar.Location = new System.Drawing.Point(279, 394);
+            this.btnVoltar.Name = "btnVoltar";
+            this.btnVoltar.Size = new System.Drawing.Size(273, 59);
+            this.btnVoltar.TabIndex = 29;
+            this.btnVoltar.Text = "Voltar";
+            this.btnVoltar.UseVisualStyleBackColor = false;
+            // 
+            // btnRemoverConta
+            // 
+            this.btnRemoverConta.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.btnRemoverConta.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRemoverConta.Location = new System.Drawing.Point(279, 306);
+            this.btnRemoverConta.Name = "btnRemoverConta";
+            this.btnRemoverConta.Size = new System.Drawing.Size(273, 62);
+            this.btnRemoverConta.TabIndex = 28;
+            this.btnRemoverConta.Text = "Remover Conta";
+            this.btnRemoverConta.UseVisualStyleBackColor = false;
+            // 
+            // GerenciarConta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(804, 465);
+            this.Controls.Add(this.btnVoltar);
+            this.Controls.Add(this.btnRemoverConta);
+            this.Controls.Add(this.dateNascimento_Conta);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txbUserNameConta);
+            this.Controls.Add(this.txbEmailConta);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Name = "GerenciarConta";
+            this.Text = "GerenciarConta";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txbUserNameConta;
+        private System.Windows.Forms.TextBox txbEmailConta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dateNascimento_Conta;
+        private System.Windows.Forms.Button btnVoltar;
+        private System.Windows.Forms.Button btnRemoverConta;
+    }
+}
